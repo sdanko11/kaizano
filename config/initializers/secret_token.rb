0b144e2595728e1ba0f22cdcca55ba35920e7691
steve <stevedanko1@gmail.com>
2013-12-24 16:59:01 -0500

    set up development enviorment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaizano::Application.config.secret_key_base = 'ba50827529da4d5b2094db83045a46048407aaf6978ca704bee240841091ae2855b10fa038a23e1448413421d38ff221cec7d07831089b913ea047c103e1d771'
