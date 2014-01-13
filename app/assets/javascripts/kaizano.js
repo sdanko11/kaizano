0affe1f282bb81eb0ffca6844d6cdf3e7db9aab5
steve <stevedanko1@gmail.com>
2014-01-13 08:52:20 -0500

    added redirect upon login

--- app/assets/javascripts/kaizano.js
+++ app/assets/javascripts/kaizano.js
@@ -5,13 +5,6 @@ $('.comment-feed').on('click', '.show-comments', function(event) {
   $comments.toggle(300);
 });
 
-$('.comment-feed').on('click', '.show-comments', function(event) {
-  event.preventDefault();
-  $showCommentsLink = $(event.target);
-  $comments = $showCommentsLink.parent().find('.comment-feed-comments');
-  $comments.toggle(300);
-});
-
 $('.add-comment').click(function(e){
   e.preventDefault();
   $(e.target.parentElement).find('.comment-form').toggle(300);
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 before_filter :update_sanitized_params, if: :devise_controller?
 
-def update_sanitized_params
-  devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:first_name, :last_name, :email, :password, 
-  :encrypted_password, :twitter_handle, :linked_in_url, :phone_number, :avatar )}
-end
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:first_name, :last_name, :email, :password, 
+    :encrypted_password, :twitter_handle, :linked_in_url, :phone_number, :avatar ) }
+  end
 
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+  
 end
