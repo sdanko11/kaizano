6e15e6b1207f00638d896985f19bc1a2d6154168
steve <stevedanko1@gmail.com>
2014-01-21 10:18:22 -0500

    fixed bug with event show page

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :event
   validates_presence_of :body
-  has_many :question_votes
-  has_many :question_comments
+  has_many :question_comments, dependent: :destroy
 
   def format_comments
     binding.pry
