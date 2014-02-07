6ff7f213bdaf1fdb4e49bedc029dfee92154df71
steve <stevedanko1@gmail.com>
2014-02-07 18:52:32 -0500

    fixed bug with multi choice questions with no answer

--- app/controllers/multi_choice_answers_controller.rb
+++ app/controllers/multi_choice_answers_controller.rb
@@ -3,11 +3,15 @@ class MultiChoiceAnswersController < ApplicationController
   def create
     @multi_choice_question = MultiChoiceQuestion.find(params[:multi_choice_question_id])
     @event = Event.find(@multi_choice_question.event_id)
-    @multi_choice_answer = @multi_choice_question.multi_choice_answers.build(multi_choice_answer_params)
-    if @multi_choice_answer.save
-      redirect_to event_multi_choice_question_path(@event, @multi_choice_question)
+    if params[:multi_choice_answer]
+      @multi_choice_answer = @multi_choice_question.multi_choice_answers.build(multi_choice_answer_params)
+      if @multi_choice_answer.save
+        redirect_to event_multi_choice_question_path(@event, @multi_choice_question)
+      else
+        render :new
+      end
     else
-      render :new
+      redirect_to :back
     end
   end
 
