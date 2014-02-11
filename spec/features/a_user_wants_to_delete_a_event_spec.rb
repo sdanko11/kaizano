78a94740d0a1af52c6f4c14a500efce7d994180d
steve <stevedanko1@gmail.com>
2014-02-11 11:37:50 -0500

    added to delete dependencies method

--- spec/features/a_user_wants_to_delete_a_event_spec.rb
+++ spec/features/a_user_wants_to_delete_a_event_spec.rb
@@ -34,15 +34,30 @@ describe 'as a registered user I want to delete a event' do
       review1 = FactoryGirl.create(:review, rating: 4, event: event3)
       review2 = FactoryGirl.create(:review, rating: 10, event: event3)
       review3 = FactoryGirl.create(:review, rating: 2, event: event3)
-      review1 = FactoryGirl.create(:review, rating: 8, event: event3)
-      review2 = FactoryGirl.create(:review, rating: 2, event: event3)
-      review3 = FactoryGirl.create(:review, rating: 8, event: event3)
+      review4 = FactoryGirl.create(:review, rating: 8, event: event3)
+      review5 = FactoryGirl.create(:review, rating: 2, event: event3)
+      review6 = FactoryGirl.create(:review, rating: 8, event: event3)
       question1 = FactoryGirl.create(:question, event: event3)
       question2 = FactoryGirl.create(:question, event: event3)
       question3 = FactoryGirl.create(:question, event: event3)
-      question3 = FactoryGirl.create(:question, event: event3)
+      question4 = FactoryGirl.create(:question, event: event3)
       question_comment = FactoryGirl.create(:question_comment, question: question3)
       question_answer = FactoryGirl.create(:question_answer, question: question1)
+      multi_choice_question1 = FactoryGirl.create(:multi_choice_question, event: event3)
+      multi_choice_answer1 = FactoryGirl.create(:multi_choice_answer, 
+        multi_choice_question: multi_choice_question1)
+      multi_choice_answer2 = FactoryGirl.create(:multi_choice_answer, 
+        multi_choice_question: multi_choice_question1)
+      multi_choice_answer3 = FactoryGirl.create(:multi_choice_answer, 
+        multi_choice_question: multi_choice_question1)
+
+      expect(Event.count).to eql(1)
+      expect(Review.count).to eql(6)
+      expect(Question.count).to eql(4)
+      expect(QuestionComment.count).to eql(1)
+      expect(QuestionAnswer.count).to eql(1)
+      expect(MultiChoiceQuestion.count).to eql(1)
+      expect(MultiChoiceAnswer.count).to eql(3)
 
       visit root_path
       click_link 'Sign In'
@@ -53,9 +68,13 @@ describe 'as a registered user I want to delete a event' do
       click_link "Delete Event"
     
       expect(page).to have_content "Deleted Event"
+      expect(Event.count).to eql(0)
       expect(Review.count).to eql(0)
+      expect(Question.count).to eql(0)
       expect(QuestionComment.count).to eql(0)
       expect(QuestionAnswer.count).to eql(0)
+      expect(MultiChoiceQuestion.count).to eql(0)
+      expect(MultiChoiceAnswer.count).to eql(0)
 
     end
 
